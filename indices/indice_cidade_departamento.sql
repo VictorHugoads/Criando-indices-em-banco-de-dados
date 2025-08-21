cd8bfba669eea0e56144dd853a11540c720ddbb1
Victor Hugo <108099384+VictorHugoads@users.noreply.github.com>
2025-08-21 10:08:27 -0300

    Update indice_cidade_departamento.sql

--- indices/indice_cidade_departamento.sql
+++ indices/indice_cidade_departamento.sql
@@ -1,1 +1,3 @@
+indice_cidade_departamento.sql: Criação do índice para otimizar a consulta de departamentos por cidade.
 
+CREATE INDEX idx_cidade_departamento ON funcionarios(cidade, departamento);
